3dd1aff8601355e8ceab6aa1fcf6a659d525c88d
Bhanu-Verma <bhanu.verma.cse22@itbhu.ac.in>
2026-04-11 10:48:54 +0000

    Implemented routing logic

--- src/router.cc
+++ src/router.cc
@@ -1,6 +1,7 @@
 #include "router.hh"
 #include "debug.hh"
 
+#include <cstdint>
 #include <iostream>
 
 using namespace std;
@@ -16,15 +17,53 @@ void Router::add_route( const uint32_t route_prefix,
                         const optional<Address> next_hop,
                         const size_t interface_num )
 {
-  cerr << "DEBUG: adding route " << Address::from_ipv4_numeric( route_prefix ).ip() << "/"
-       << static_cast<int>( prefix_length ) << " => " << ( next_hop.has_value() ? next_hop->ip() : "(direct)" )
-       << " on interface " << interface_num << "\n";
-
-  debug( "unimplemented add_route() called" );
+  routing_table_.emplace_back( route_prefix, prefix_length, next_hop, interface_num );
 }
 
 // Go through all the interfaces, and route every incoming datagram to its proper outgoing interface.
 void Router::route()
 {
-  debug( "unimplemented route() called" );
+  for ( auto& i : interfaces_ ) {
+    auto& buffer = i->datagrams_received();
+    while ( !buffer.empty() ) {
+      auto dgrm = buffer.front();
+      buffer.pop();
+
+      uint8_t longest_match = 0;
+      std::optional<Address> nh {};
+      size_t in;
+      bool found = false;
+
+      for ( const auto& route : routing_table_ ) {
+        if ( is_match( route, dgrm.header.dst ) && route.prefix_length >= longest_match ) {
+          nh = route.next_hop;
+          in = route.interface_num;
+          longest_match = route.prefix_length;
+          found = true;
+        }
+      }
+
+      if ( !found || dgrm.header.ttl == 0 ) {
+        continue;
+      }
+
+      --dgrm.header.ttl;
+      if ( dgrm.header.ttl == 0 ) {
+        continue;
+      }
+
+      dgrm.header.compute_checksum();
+      if ( nh ) {
+        interface( in )->send_datagram( dgrm, *nh );
+      } else {
+        interface( in )->send_datagram( dgrm, Address::from_ipv4_numeric( dgrm.header.dst ) );
+      }
+    }
+  }
+}
+
+bool Router::is_match( const Route& route, uint32_t addr ) const
+{
+  uint32_t mask = UINT32_MAX ^ ( ( 1LL << ( 32 - route.prefix_length ) ) - 1 );
+  return ( ( route.route_prefix & mask ) == ( addr & mask ) );
 }
--- src/router.hh
+++ src/router.hh
@@ -10,6 +10,19 @@
 class Router
 {
 public:
+  struct Route
+  {
+    uint32_t route_prefix;
+    uint8_t prefix_length;
+    std::optional<Address> next_hop;
+    size_t interface_num;
+
+    Route( uint32_t rp, uint8_t pl, std::optional<Address> nh, size_t in )
+      : route_prefix { rp }, prefix_length { pl }, next_hop { nh }, interface_num { in }
+    {}
+  };
+
+public:
   // Add an interface to the router
   // \param[in] interface an already-constructed network interface
   // \returns The index of the interface after it has been added to the router
@@ -34,4 +47,6 @@ public:
 private:
   // The router's collection of network interfaces
   std::vector<std::shared_ptr<NetworkInterface>> interfaces_ {};
+  std::vector<Route> routing_table_ {};
+  bool is_match( const Route& route, uint32_t addr ) const;
 };
